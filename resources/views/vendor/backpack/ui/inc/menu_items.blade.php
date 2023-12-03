d7cbc2a18df46c2b75820c81d96213ba1a81d0a6
Tom Van den Eynden <tom.vandeneynden@calibrate.be>
2023-12-03 15:38:08 +0100

    Remove subposts as approachable Backpack resource

--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -2,4 +2,3 @@
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 
 <x-backpack::menu-item title="Posts" icon="la la-question" :link="backpack_url('post')" />
-<x-backpack::menu-item title="Sub posts" icon="la la-question" :link="backpack_url('sub-post')" />
--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -17,5 +17,4 @@ Route::group([
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
     Route::crud('post', 'PostCrudController');
-    Route::crud('sub-post', 'SubPostCrudController');
 }); // this should be the absolute last line of this file
